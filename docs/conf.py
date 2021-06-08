dbf89576d2e9df4ae546a1c17b66a52b59b0f786
adamgayoso <adamgayoso@users.noreply.github.com>
2021-06-08 14:32:06 -0700

    use tango pygments

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,7 @@ language = None
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "colorful"
+pygments_style = "tango"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
